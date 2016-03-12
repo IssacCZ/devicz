e3417feedd9bd88321e15b30b78c3a4b7d915444
IssacCZ <sheldowkay@icloud.com>
2016-03-12 23:13:52 +0800

    init

---
+++ www/static/orm.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import asyncio, logging
+import aiomysql
+def log(sql, args = ()):
+    logging.info('SQL: %s' % sql)
+
+async def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host = kw.get('host', 'localhost'),
+        port = kw.get('port', 3306),
+        user = kw.get['user']
+        password = kw['password']
+        db = kw['db']
+        charset = kw.get('charset', 'utf-8'),
+        autocommit = kw.get('autocommit', True),
+        maxsize = kw.get('maxsize', 10),
+        minsize = kw.get('minsize', 1),
+        loop = loop
+    )
+
+async def select(sql, args, size = None):
+    log(sql, args)
+    global __pool
+    async with __pool.get() as conn:
+        async with conn.cursor(aiomysql.DirtCursor) as cur:
+            await cur.execute(sql.replace('?', '%s'), args or())
+            if size:
+                rs = await cur.getchmany()
+            else:
+                rs = await cur.getchall()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
+
+
